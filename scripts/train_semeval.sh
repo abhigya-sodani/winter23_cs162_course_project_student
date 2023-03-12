8cfe6c35f2c3cabbc326f17b71b86813662b9edc
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2023-03-12 15:27:48 -0700

    testing

--- scripts/train_semeval.sh
+++ scripts/train_semeval.sh
@@ -13,7 +13,7 @@ python3 -m trainers.train \
   --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 796 \
   --learning_rate 1e-5 \
-  --max_steps 4000 \
+  --max_steps 0 \
   --num_train_epochs 2 \
   --max_seq_length 128 \
   --output_dir "${TASK_NAME}/ckpts" \
