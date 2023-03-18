099a5b53df41902f60bf61c918ca2303b94c5306
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2023-03-18 13:25:49 -0700

    eval script added

--- scripts/eval_com2sense.sh
+++ scripts/eval_com2sense.sh
@@ -4,14 +4,16 @@ MODEL_TYPE="roberta-base"
 
 
 python3 -m trainers.eval.py \
+  --model_name_or_path outputs/com2sense/ckpts/ckpts/checkpoint-8000 \
   --do_not_load_optimizer \
   --do_train \
   --do_eval \
   --evaluate_during_training \
   --gradient_accumulation_steps 4 \
+  --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 796 \
   --learning_rate 1e-5 \
-  --model_name_or_path outputs/com2sense/ckpts/ckpts/checkpoint-8000
+  --num_train_epochs 1 \
   --max_seq_length 128 \
   --output_dir "${TASK_NAME}/ckpts" \
   --task_name "${TASK_NAME}" \
@@ -23,4 +25,5 @@ python3 -m trainers.eval.py \
   --eval_split "dev" \
   --score_average_method "binary" \
   --iters_to_eval 10000 \
+  --overwrite_output_dir \
   # --max_eval_steps 1000 \
